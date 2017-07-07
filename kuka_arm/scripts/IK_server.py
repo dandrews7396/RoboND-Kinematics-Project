9e1fe31f97b0d71357fd7821bd6afb0b36e20325
dandrews7396 <danielandrews7396@gmail.com>
2017-07-07 07:22:54 -0700

    Add Robo-nd folder

--- kuka_arm/scripts/IK_server.py
+++ kuka_arm/scripts/IK_server.py
@@ -33,47 +33,170 @@ def handle_calculate_IK(req):
 
             # Define DH param symbols
 
-
-            
+            q1, q2, q3, q4, q5, q6, q7 = symbols('q1:8')
+            d1, d2, d3, d4, d5, d6, d7 = symbols('d1:8')
+            a0, a1, a2, a3, a4, a5, a6 = symbols('a0:7')
+            alpha0, alpha1, alpha2, alpha3, alpha4, alpha5, alpha6 = symbols('alpha0:7')
             # Joint angle symbols
+            r, p, y = symbols('r p y')    # thetas for EEf
 
 
-      
             # Modified DH params
 
 
-            
-            # Define Modified DH Transformation matrix
+            s = {alpha0: 0,        a0: 0,        d1: 0.75,
+                alpha1: -pi/2,    a1: 0.35,     d2: 0,
+                alpha2: 0,        a2: 1.25,     d3: 0,        q2: q2-(pi/2 + 0.06),
+                alpha3: -pi/2,    a3: -0.54,    d4: 1.50,     q3: q3 - 0.21099,
+                alpha4: pi/2,     a4: 0,        d5: 0,
+                alpha5: -pi/2,    a5: 0,        d6: 0,
+                alpha6: 0,        a6: 0,        d7: 0.303,    q7: 0}
 
+            # Define Modified DH Transformation matrix
+            T0_1 = Matrix([[            cos(q1),            -sin(q1),            0,              a0],
+               [sin(q1)*cos(alpha0), cos(q1)*cos(alpha0), -sin(alpha0), -sin(alpha0)*d1],
+               [sin(q1)*sin(alpha0), cos(q1)*sin(alpha0),  cos(alpha0),  cos(alpha0)*d1],
+               [                  0,                   0,            0,               1]])
+            T1_2 = Matrix([[            cos(q2),            -sin(q2),            0,              a1],
+               [sin(q2)*cos(alpha1), cos(q2)*cos(alpha1), -sin(alpha1), -sin(alpha1)*d2],
+               [sin(q1)*sin(alpha0), cos(q1)*sin(alpha0),  cos(alpha0),  cos(alpha0)*d2],
+               [                  0,                   0,            0,               1]])
+            T2_3 = Matrix([[            cos(q3),            -sin(q3),            0,              a2],
+               [sin(q3)*cos(alpha2), cos(q3)*cos(alpha2), -sin(alpha2), -sin(alpha2)*d3],
+               [sin(q3)*sin(alpha2), cos(q3)*sin(alpha2),  cos(alpha2),  cos(alpha2)*d3],
+               [                  0,                   0,            0,               1]])
+            T3_4 = Matrix([[            cos(q4),            -sin(q4),            0,              a3],
+               [sin(q4)*cos(alpha3), cos(q4)*cos(alpha3), -sin(alpha3), -sin(alpha3)*d4],
+               [sin(q4)*sin(alpha3), cos(q4)*sin(alpha3),  cos(alpha3),  cos(alpha3)*d4],
+               [                  0,                   0,            0,               1]])
+            T4_5 = Matrix([[            cos(q5),            -sin(q1),            0,              a4],
+               [sin(q5)*cos(alpha4), cos(q5)*cos(alpha4), -sin(alpha4), -sin(alpha4)*d5],
+               [sin(q5)*sin(alpha4), cos(q5)*sin(alpha4),  cos(alpha4),  cos(alpha4)*d5],
+               [                  0,                   0,            0,               1]])
+            T5_6 = Matrix([[            cos(q6),            -sin(q6),            0,              a5],
+               [sin(q6)*cos(alpha5), cos(q6)*cos(alpha5), -sin(alpha5), -sin(alpha5)*d6],
+               [sin(q6)*sin(alpha5), cos(q6)*sin(alpha5),  cos(alpha5),  cos(alpha5)*d6],
+               [                  0,                   0,            0,               1]])
+            T6_G = Matrix([[            cos(q7),            -sin(q7),            0,              a6],
+               [sin(q7)*cos(alpha6), cos(q7)*cos(alpha6), -sin(alpha6), -sin(alpha6)*d7],
+               [sin(q7)*sin(alpha6), cos(q7)*sin(alpha6),  cos(alpha6),  cos(alpha6)*d7],
+               [                  0,                   0,            0,               1]])
 
 
             # Create individual transformation matrices
+            T0_1 = T0_1.subs(s)
+            T1_2 = T1_2.subs(s)
+            T2_3 = T2_3.subs(s)
+            T3_4 = T3_4.subs(s)
+            T4_5 = T4_5.subs(s)
+            T5_6 = T5_6.subs(s)
+            T6_G = T6_G.subs(s)
 
+            # Transform from base link to EEf
+            T0_2 = simplify(T0_1 * T1_2)
+            T0_3 = simplify(T0_2 * T2_3)
+            T0_4 = simplify(T0_3 * T3_4)
+            T0_5 = simplify(T0_4 * T4_5)
+            T0_6 = simplify(T0_5 * T5_6)
+            T0_G = simplify(T0_6 * T6_G)
 
-            
+            ## Apply Correction for Orientation difference between Gripper_link URDF vs D-H
+            R_z = Matrix([[cos(q3), -sin(q3),   0, 0],
+              [sin(q3), cos(q3),    0, 0],
+              [0,       0,          1, 0],
+              [0,       0,          0, 1]])
+
+            R_y = Matrix([[cos(q2),     0,  sin(q2),    0],
+              [0,           1,  0,          0],
+              [-sin(q2),    0,  cos(q2),    0],
+              [0,           0,  0,          0]])
+
+            R_corr = simplify(R_z * R_y)
+
             # Extract end-effector position and orientation from request
-	    # px,py,pz = end-effector position
-	    # roll, pitch, yaw = end-effector orientation
+            # px,py,pz = end-effector position
+            # roll, pitch, yaw = end-effector orientation
             px = req.poses[x].position.x
             py = req.poses[x].position.y
             pz = req.poses[x].position.z
 
             (roll, pitch, yaw) = tf.transformations.euler_from_quaternion(
-                [req.poses[x].orientation.x, req.poses[x].orientation.y,
-                    req.poses[x].orientation.z, req.poses[x].orientation.w])
-     
-            # Calculate joint angles using Geometric IK method
+            [req.poses[x].orientation.x, req.poses[x].orientation.y,
+            req.poses[x].orientation.z, req.poses[x].orientation.w])
+            T0_6mtrx = tf.transformations.quaternion_matrix([req.poses[x].orientation.x,
+                                                 req.poses[x].orientation.y,
+                                                 req.poses[x].orientation.z,
+                                                 req.poses[x].orientation.w])
 
-		
+            wx = px - d7 * T0_6mtrx[0][2]
+            wy = py - d7 * T0_6mtrx[1][2]
+            wz = pz - d7 * T0_6mtrx[2][2]
+
+            
+            # Calculate joint angles using Geometric IK method
+            theta1 = atan2(wy, wx) # Simple trig, using x,y coords
+
+            # Find 'r' of origin(O) to wc
+            r = sqrt(wx**2 + wy**2)
+
+            # Calculate dist J2 - wc (c) and angle from horizontal to wc (C)
+            a = r - a1  
+            b = wz - d1
+            c = sqrt(a**2 + b**2) ## Length J2 - wc
+
+            # Use cosine rule to find theta3 and allow for offset
+            costheta3 = (a2**2 + d4**2 - c**2)/(2 * a2 * d4)
+            #so
+            theta3 = atan2(costheta3, sqrt(1 - costheta3**2))
+
+            # Find elevation angle from J2 to J5
+            theta21 = atan2(b, a)
+            # Then angle between a2 and c (length J2 - J5)
+            # And hence, total of that angle
+            theta2 = theta21 + atan2(a2 + d4*cos(theta3), d4*sin(theta3))
+
+            ## Find thetas for q4, q5 & q6
+            ## Solve for T0_3
+            T0_3mtrx = T0_3.evalf(subs={q1: theta1, q2: theta2, q3: theta3})
+
+            ## Use T0_3 with T0_6 from request data to find T3_6
+            T3_6mtrx = inv(T0_3mtrx) * T0_6mtrx * R_corr
+
+            ## Assign matrix elements to variables
+            r11 = T3_6mtrx[0,0]
+            r12 = T3_6mtrx[0,1]
+            r13 = T3_6mtrx[0,2]
+            r21 = T3_6mtrx[1,0]
+            r31 = T3_6mtrx[2,0]
+            r32 = T3_6mtrx[2,1]
+            r33 = T3_6mtrx[2,2]
+
+            theta5 = atan2(-r31, sqrt(r11*r11 + r21*r21))
+
+            # Check for lock within the wrist,
+            # pitch at -90
+            if r31 == 1:
+                theta4 = 0.0
+                theta6 = atan2(-r12, -r13)
+
+            # pitch at 90
+            elif r31 == -1:
+                theta4 = 0.0
+                theta6 = atan2(r12, r13)
+
+            else:
+                theta4 = atan2(r21, r11)
+                theta6 = atan2(r32, r33)
+
 
 
             # Populate response for the IK request
             # In the next line replace theta1,theta2...,theta6 by your joint angle variables
-	    joint_trajectory_point.positions = [theta1, theta2, theta3, theta4, theta5, theta6]
-	    joint_trajectory_list.append(joint_trajectory_point)
+            joint_trajectory_point.positions = [theta1, theta2, theta3, theta4, theta5, theta6]
+            joint_trajectory_list.append(joint_trajectory_point)
 
-        rospy.loginfo("length of Joint Trajectory List: %s" % len(joint_trajectory_list))
-        return CalculateIKResponse(joint_trajectory_list)
+            rospy.loginfo("length of Joint Trajectory List: %s" % len(joint_trajectory_list))
+    return CalculateIKResponse(joint_trajectory_list)
 
 
 def IK_server():
--- writeup_template.md
+++ writeup_template.md
@@ -1,5 +1,5 @@
-## Project: Kinematics Pick & Place
-### Writeup Template: You can use this file as a template for your writeup if you want to submit it as a markdown file, but feel free to use some other method and submit a pdf if you prefer.
+# Project: Kinematics Pick & Place
+### This writeup will attept to explain the processes used to complete the Kinematics Pick & Place Project for the Udacity Robotics Nano-Degree.
 
 ---
 
@@ -17,7 +17,7 @@
 
 [//]: # (Image References)
 
-[image1]: ./misc_images/misc1.png
+[image1]: ./misc_images/fwd_kin.png
 [image2]: ./misc_images/misc2.png
 [image3]: ./misc_images/misc3.png
 
@@ -27,18 +27,71 @@
 ---
 ### Writeup / README
 
-#### 1. Provide a Writeup / README that includes all the rubric points and how you addressed each one.  You can submit your writeup as markdown or pdf.  
+#### 1. Provide a Writeup / README that includes all the rubric points and how you addressed each one.  
 
-You're reading it!
+This Writeup should fulfill the requirement of the first Rubric
 
 ### Kinematic Analysis
 #### 1. Run the forward_kinematics demo and evaluate the kr210.urdf.xacro file to perform kinematic analysis of Kuka KR210 robot and derive its DH parameters.
 
-Here is an example of how to include an image in your writeup.
+On inspecting the `kr210.urdf.xacro` file supplied within the project folders, it is possible to extract the physical data for the arm. This includes joint origin co-ordinates (relative to the previous joint), joint rotation limits and the axis of rotation, which can be used to create a Denavit-Hartenberg (D-H) parameter table.
+In doing this, I first created a table of data for each link and joint:
 
-![alt text][image1]
+Joint | x-dist | y-dist | z-dist | axis
+--- | --- | --- | --- | ---
+1 | 0.0 | 0.0 | 0.33 | z
+2 | 0.35 | 0.0 | 0.42 | y
+3 | 0.0 | 0.0 | 1.25 | y
+4 | 0.96 | 0.0 | -0.54 | x
+5 | 0.54 | 0.0 | 0.0 | y
+6 | 0.193 | 0.0 | 0.0 | x
+G | 0.11 | 0.0 | 0.0 | y
+
+In transforming this data into D-H values, the following rules were followed:
+
+	1. In describing reference frames for each joint, the z-axis is along the joint rotational axis.
+	2. common normals should be identified between joint reference frames.
+	3. Where possible parameters should ideally be zero.
+	4. Where links are coincident with joint axes, the sum of the link lengths is to be assigned to the link furthest from the last perpendicular joint axis.
+
+This enabled me to produce the following D-H Parameter Table:
+
+i | \alpha~i-1 | a~i-1~ | d~i~ | \theta
+--- | --- | --- | --- | ---
+T^0^~1~ | 0 | 0 | 0.75 | -
+T^1^~2~ | -\pi/2| 0.35 | 0 | -
+T^2^~3~ | 0 | 1.25 | 0 | \theta~2~ - \pi/2
+T^3^~4~ | -\pi/2 | -0.54 | 1.50 | -
+T^4^~5~ | \pi/2 | 0 | 0 | -
+T^5^~6~ | -\pi/2 | 0 | 0 | -
+T^6^~G~ | 0 | 0 | 0.303 | 0
+
+
 
 #### 2. Using the DH parameter table you derived earlier, create individual transformation matrices about each joint. In addition, also generate a generalized homogeneous transform between base_link and gripper_link using only end-effector(gripper) pose.
+
+The above D-H Parameter table was used to create transformation matrices for each of the joints, all of which took the following form:
+
+Tn~-1~_n =
+[cos(n) | -sin(n) | 0 | a n~-1~]
+[sin(n)*cos(alpha n~-1~) | cos(n)*cos(alpha n~-1~) | -sin(alpha n~-1~) | -sin(alpha n~-1~)* d n]
+[sin(n)*sin(alpha n~-1~) | cos(n)*sin(alpha n~-1~) | cos(alpha n~-1~) | cos(alpha n~-1~) * d n]
+[0 | 0 | 0 | 1]
+
+Once give angles for each of the joints, these individual transformation matrices could then be used to produce a matrix for the entire arm. This is done via multiplication of each matrix in series, to produce a final matrix of the form:
+
+R0_G ||| Px
+--- | --- | --- |---
+|||| Py
+|||| Pz
+0 | 0 | 0 | 1
+
+Where Px Py and Pz are the co-ordinates of the gripper link, and R0_6 is the rotation matrix the describes it's orientation.
+
+It is at this point that I used the `forward_kinematics.launch` program and `rosrun tf tf_echo`, together with my own code to produce a forward kinematics model, to adjust for any errors between Rviz* and my calculations.
+
+![alt text][image1]
+Using Rviz* to perform error correction
 
 Here's | A | Snappy | Table
 --- | --- | --- | ---
@@ -48,6 +101,24 @@ Here's | A | Snappy | Table
 4 | 2 | 3 | abcd
 
 #### 3. Decouple Inverse Kinematics problem into Inverse Position Kinematics and inverse Orientation Kinematics; doing so derive the equations to calculate all individual joint angles.
+
+In order to solve the problem of Inverse Kinematics for this project, it was first necessary to split it into two smaller problems; Position Kinematics for Joints 1,2 and 3, and Orientation Kinematics for Joints 4,5 and 6.
+In doing this, we effectively split the arm into two sections. The shoulder and elbow section, containing joints 1, 2 and 3, and the wrist section containing joints 4,5 and 6. We refer to this as a wrist joint due to their collective behaviour, which is similar to the human wrist, and for this reason we can effectively treat them as a single, double-revolute joint.
+So to start, it is necessary to find the position of the 'wrist centre' (actually the centre of Joint 4). This is achieved by using the Positional co-ordinates and z-axis components from the overall tranformation matrix. These are input into the following formulae:
+
+	* Wx = Px - d7 * Nx
+	* Wy = Py - d7 * Ny
+	* Wz = Pz - d7 * Ny
+
+From here, it is possible to find Joints 1, 2 and 3 via the use of triganometric rules.
+
+For Joint 1, pythagoras' theorem:
+
+	* a^2^ = b^2^ + c^2^
+
+Which is adapted to solve for \theta1 from Wx and Wy, using the arctangent:
+
+	* \theta1 = `atan2(Wy, Wx)`
 
 And here's another image! 
 
